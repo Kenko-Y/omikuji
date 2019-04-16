77c024ed932583c58f2b96b2d90c9dcc2918ffee
Kenko <yang_jian_fang@yahoo.co.jp>
2019-04-16 11:00:09 +0800

    add lucky color.

--- js_omikuji-master/assets/js/app.js
+++ js_omikuji-master/assets/js/app.js
@@ -1,20 +1,71 @@
+'use strict';
 /**********************************************************
  *  Omikuji App
  **********************************************************/
-/* --- クリックイベント --- */
-// btn.addEventListener('click', omikuji);
-btn.onclick = omikuji;
+window.onload = function () {
+    /* --- 変数宣言 --- */
+    // const result = document.getElementsByClassName('result');
+    const btn = document.getElementById('btn');
+    // const btn_co = document.getElementById('btn_co');
+    let state = 0;
 
+    /* --- クリックイベント --- */
+    btn.addEventListener('click', function () {
+        state %= 3;
+        omikuji(state, btn);
+        state++;
+    });
+    // btn_om.onclick = function () {
+    //     state = state++ % 3;
+    //     omikuji(state);
+    // };
+    // btn_co.addEventListener('click', chooseLukyColor);
+}
 
 /* --- おみくじアプリ（本体） --- */
-function omikuji(state) {
+function omikuji(state, btn) {
     // 定数定義
-    const outpt = ["大吉", "中吉", "小吉", "末吉","吉", "凶","大凶"];
+    const outpt = document.getElementById("result");
+    const results_lt = ["大吉", "中吉", "末吉", "吉", "凶"];
 
     // ランダム変数生成
-    const n = Math.floor(Math.random() * 7);   /* random create -> 0-6 */
+    const n = Math.floor(Math.random() * results_lt.length);   /* create random */
+    switch (state) {
+        case 0:
+            outpt.innerHTML = results_lt[n];
+            btn.innerHTML = "<span>ラッキーカラーは…</span>";
+            console.log(`今日の運勢は...${results_lt[n]}`);
+            break;
+        case 1:
+            outpt.innerHTML = chooseLukyColor();
+            btn.innerHTML = "<span>もう一度占う</span>";
+            break;
+        case 2:
+            location.reload();
+            break;
+        default:
+            console.log("Error statement!!");
+            break;
+    }
+    // console.log(state);
+    return 0;
+};
 
-    const output = document.getElementById("result");
+/* --- ラッキーカラー選定 --- */
+function chooseLukyColor() {
+    // 定数定義
+    const results_conm = ["Red", "Blue", "Green", "Black","Gray","Pink","Lime","Navy","Gold","Silver"];
+    const addoutpt = document.getElementsByClassName("result");
 
-    output.innerText = outpt[state];
+    // ランダム変数生成
+    const n = Math.floor(Math.random() * results_conm.length);   /* create random */
+
+    for (const aout of addoutpt) {
+
+        aout.style.backgroundColor = results_conm[n];
+
+    }
+    // console.log(results_conm[n]);
+
+    return results_conm[n];
 };
